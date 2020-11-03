dd37f674e8a47495b17ccd8a9eb3b004af186ee4
ggujangi <asmwj3@naver.com>
2020-11-03 17:24:51 +0900

    Submit avd list data

--- app/src/main/java/com/ggu/avd/ui/list/ListFragment.kt
+++ app/src/main/java/com/ggu/avd/ui/list/ListFragment.kt
@@ -1,32 +1,56 @@
 package com.ggu.avd.ui.list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.ggu.avd.R
+import com.ggu.avd.databinding.FragmentListBinding
+import com.ggu.avd.utilities.provideDrawableRepository
+
 
 class ListFragment : Fragment() {
 
-    private lateinit var listViewModel: ListViewModel
+    private lateinit var viewModel:ListViewModel
+
     private val args: ListFragmentArgs by navArgs()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        listViewModel =
-            ViewModelProvider(this).get(ListViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_list, container, false)
-        listViewModel.text.observe(viewLifecycleOwner, Observer {
-        })
+        val binding = FragmentListBinding.inflate(inflater, container, false)
+        binding.avdTitle.text = findNavController().currentDestination?.label
 
-        return root
+        context?.let { context->
+            viewModel = ViewModelProvider(this, object : ViewModelProvider.Factory {
+                override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                    return provideDrawableRepository(context)?.let { ListViewModel(it) } as T
+                }
+            }).get(ListViewModel::class.java)
+
+            val listAdapter = DrawableListAdapter()
+
+            binding.avdList.apply {
+                adapter = listAdapter
+                setHasFixedSize(true)
+            }
+
+            subscribeUi(listAdapter)
+        }
+
+        return binding.root
     }
+
+    private fun subscribeUi(adapter: DrawableListAdapter) {
+        viewModel.drawables.observe(viewLifecycleOwner) { drawables ->
+            adapter.submitList(drawables)
+        }
+    }
+
 }
--- app/src/main/java/com/ggu/avd/ui/list/ListViewModel.kt
+++ app/src/main/java/com/ggu/avd/ui/list/ListViewModel.kt
@@ -3,12 +3,15 @@ package com.ggu.avd.ui.list
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ggu.avd.data.AvdDrawable
+import com.ggu.avd.data.DrawableRepository
+import com.squareup.inject.assisted.AssistedInject
 
-class ListViewModel : ViewModel() {
+class ListViewModel internal constructor(
+        drawableRepository: DrawableRepository
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
-    }
+    val drawables:LiveData<List<AvdDrawable>> = drawableRepository.getDrawables()
 
-    val text: LiveData<String> = _text
 }
